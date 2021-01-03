447216d1c48c853803a401095666027f11145ee6
Nico Bachner <yo@nicob.dev>
2021-01-03 18:30:11 +0100

    converted images to Nextjs images components

--- components/project.jsx
+++ components/project.jsx
@@ -1,10 +1,17 @@
 import { GitHub, Website } from "./icons";
+import Image from 'next/image'
 
 export function ProjectLeft(props) {
     return (
         <div className="py-12 w-full grid grid-cols-12 grid-rows-1">
             <div className="col-start-6 col-span-7 row-span-1 row-start-1">
-                <img src={"images/projects/" + props.image} alt={"Image of" + " " + props.project} className="w-full border border-blue-300 dark:border-blue-700 rounded"/>
+                <Image 
+                    src={"/images/projects/" + props.image}
+                    alt={"Image of" + " " + props.project}
+                    width={2800}
+                    height={1550}
+                    className="w-full border border-blue-300 dark:border-blue-700 rounded"
+                />
             </div>
             <div className="z-20 col-start-1 col-span-6 row-span-1 row-start-1">
                 <p className="text-sm text-red-500 dark:text-green-300 font-mono">Featured Project</p>
@@ -36,7 +43,13 @@ export function ProjectRight(props) {
     return (
         <div className="py-12 w-full grid grid-cols-12 grid-rows-1">
             <div className="col-start-1 col-span-7 row-span-1 row-start-1">
-                <img src={"images/projects/" + props.image} alt={"Image of" + " " + props.project} className="w-full border border-blue-300 dark:border-blue-700 rounded"/>
+            <Image 
+                    src={"/images/projects/" + props.image}
+                    alt={"Image of" + " " + props.project}
+                    width={2800}
+                    height={1550}
+                    className="w-full border border-blue-300 dark:border-blue-700 rounded"
+                />
             </div>
             <div className="z-20 text-right col-start-7 col-span-6 row-span-1 row-start-1">
                 <p className="text-sm text-red-500 dark:text-green-300 font-mono">Featured Project</p>
