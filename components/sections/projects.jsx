417f7c09d288da05dd0da27ddaf1c34c53f566bd
Nico Bachner <yo@nicob.dev>
2021-01-06 11:44:09 +0100

    fixed spacing between projects

--- components/sections/projects.jsx
+++ components/sections/projects.jsx
@@ -6,7 +6,7 @@ export default function Projects(props) {
             <h2 className="text-2xl lg:text-3xl font-bold pb-12">
                 <span className="text-red dark:text-green font-mono text-lg lg:text-xl font-normal">02.</span> What I've Built
             </h2>
-            <div className="w-full grid grid-cols-12 sm:grid-rows-3 sm:gap-y-20 gap-x-6">
+            <div className="w-full grid grid-cols-12 sm:grid-rows-3 gap-4 sm:gap-y-20">
                 <ProjectRight
                     project="CareerGuru" 
                     description="Product of a hackathon. Designed to be a web app that helps students choose their professional orientation by providing a platform for professionals and students to come together."
