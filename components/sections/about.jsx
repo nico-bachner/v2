e9f91acab815714d3d87cd7a711bc07d96f7c746
Nico Bachner <yo@nicob.dev>
2021-01-30 11:08:28 +0100

    fix(layout): removed about image placeholder

--- components/sections/about.jsx
+++ components/sections/about.jsx
@@ -8,31 +8,24 @@ export default function About(props) {
             <h2 className="text-2xl lg:text-3xl font-bold text-indigo dark:text-cyan">
                 <span className="text-red dark:text-green font-mono text-lg lg:text-xl font-normal">01.</span> About Me
             </h2>
-            <div className="mt-6 lg:grid grid-cols-3 gap-6 max-w-4xl">
-                <div className="col-span-2">
-                    <p>Hi! I'm Nico, a hobby developer currently living in Luxembourg.</p>
-                    <p className="my-4 max-w-prose">
-                        I find making things really fun. 
-                        Currently, most of my projects have been web-based, but I want to explore more areas of development as well.
-                        In the future, I would like to contribute to Open Source even more.
-                    </p>
-                    <p>Here are a few technologies I've been working with recently:</p>
-                    <ul className="grid grid-cols-2 gap-x-4 list-disc list-inside font-mono my-4">
-                        <li>JavaScript</li>
-                        <li>CSS</li>
-                        <li>HTML</li>
-                        <li>Node.js & Deno</li>
-                        <li>Svelte & SvelteKit</li>
-                        <li>React & Next.js</li>
-                        <li>Unix Shell</li>
-                    </ul>
-                    <p>I'm currently also learning <ExtLink href="https://www.rust-lang.org">Rust</ExtLink> and systems-level programming.</p>
-                </div>
-                <img
-                    src="/images/nico.png" 
-                    alt="Picture of Nico"
-                    className="hidden lg:block w-full border"
-                />
+            <div className="mt-6 gap-6">
+                <p>Hi! I'm Nico, a hobby developer currently living in Luxembourg.</p>
+                <p className="my-4 max-w-prose">
+                    I find making things really fun. 
+                    Currently, most of my projects have been web-based, but I want to explore more areas of development as well.
+                    In the future, I would like to contribute to Open Source even more.
+                </p>
+                <p>Here are a few technologies I've been working with recently:</p>
+                <ul className="grid grid-cols-2 gap-x-4 list-disc list-inside font-mono my-4">
+                    <li>JavaScript & TypeScript</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>Node.js & Deno</li>
+                    <li>Svelte & SvelteKit</li>
+                    <li>React & Next.js</li>
+                    <li>Unix Shell</li>
+                </ul>
+                <p>I'm currently also learning <ExtLink href="https://www.rust-lang.org">Rust</ExtLink> and systems-level programming.</p>
             </div>
         </Section>
     )
